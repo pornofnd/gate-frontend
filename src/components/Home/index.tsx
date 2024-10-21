e0a61c58c7685659056c0bd83d16de4706c3508b
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-21 22:17:06 +0300

    add tg auth

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Home() {
-  return <div>Home</div>;
+  return <div className="App">home</div>;
 }
---
+++ src/components/TelegramAuth/index.tsx
@@ -0,0 +1,22 @@
+import { LoginButton } from "@telegram-auth/react";
+
+export default function TelegramAuth() {
+  return (
+    <LoginButton
+      botUsername="pornoGateBot"
+      authCallbackUrl="https://gate.pornofnd.com/api/web/auth/telegram"
+      buttonSize="large" // "large" | "medium" | "small"
+      cornerRadius={5} // 0 - 20
+      showAvatar={false} // true | false
+      lang="en"
+    />
+  );
+}
+{
+  /* <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="pornoGateBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+  <script type="text/javascript">
+    function onTelegramAuth(user) {
+      alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    }
+  </script> */
+}
