9ac336ad64a23ad276782674ab07d49163526688
artem <127351345+talafarael@users.noreply.github.com>
2024-11-09 02:08:49 +0200

    tg

--- src/components/TelegramAuth/index.tsx
+++ src/components/TelegramAuth/index.tsx
@@ -1,17 +1,51 @@
 import { LoginButton } from "@telegram-auth/react";
+import React, { useState, useEffect } from "react";
+import ReactModal from "react-modal";
+import { useAuthTelegramMutation } from "store/api/ConnectAuthApi";
+import { ITelegramResponse, IToken, Response } from "type/Response";
 
-import { useAuthTelegramMutation } from "../../store/api/ConnectAuthApi";
-import { ITelegramResponse, Response } from "type/Response";
-
-// import useGetMeApi from "utils/useGetMeApi";
-
-interface IToken {
-  data:string;
-}
-export default function TelegramAuth() {
+const TelegramAuthModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); 
   const [authTelegram]=useAuthTelegramMutation()
+ 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+ 
+
   return (
-    <LoginButton
+    <div>
+      {/* Кнопка для открытия модалки */}
+      <button onClick={openModal}>Авторизация через Telegram</button>
+
+      
+      <ReactModal
+        isOpen={isModalOpen}       // Модалка открыта, если isModalOpen true
+        onRequestClose={closeModal} // Закрыть модалку при клике за её пределами
+        contentLabel="Авторизация через Telegram"
+        ariaHideApp={false}
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+            width: '400px',
+            padding: '20px',
+            borderRadius: '10px',
+          },
+        }}
+      >
+        <h2>Авторизация через Telegram</h2>
+        <div id="telegram-widget-container">
+        <LoginButton
       // @pornogatetestbot
       botUsername="pornogatetestbot"
       
@@ -28,26 +62,21 @@ export default function TelegramAuth() {
         if(data?.data){
           const token = JSON.stringify(data.data);
           localStorage.setItem("token", token);
+          closeModal()
           // useGetMeApi()
       }
-          // .then((res) => {
-          //   console.log(res);
-
-          //   localStorage.setItem("token",JSON.stringify(res.data));
-          // });
+          
       }}
-      buttonSize="large" // "large" | "medium" | "small"
-      cornerRadius={5} // 0 - 20
-      showAvatar={false} // true | false
+      buttonSize="large" 
+      cornerRadius={5} 
+      showAvatar={false}
       lang="en"
     />
+        </div>
+        <button onClick={closeModal}>Закрыть</button>
+      </ReactModal>
+    </div>
   );
-}
-{
-  /* <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="pornoGateBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-  <script type="text/javascript">
-    function onTelegramAuth(user) {
-      alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-    }
-  </script> */
-}
+};
+
+export default TelegramAuthModal;
